4caf293d3d7fec24c3cfc90849fd58428ca0e373
DaVinci <ing_sergio.l@hotmail.com>
2022-10-05 02:24:25 -0500

    fixing path

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import 'SearchBar.css';
+import './SearchBar.css';
 
 export class SearchBar extends React.Component
     {
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import 'SearchResults.css'
+import './SearchResults.css'
 
 
 export class SearchResults extends React.components
