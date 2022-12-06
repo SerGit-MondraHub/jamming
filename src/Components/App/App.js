afe4388694fe54dfb0ff29b2f26ff02bad8b2807
DaVinci <ing_sergio.l@hotmail.com>
2022-12-06 03:34:10 -0600

    import fix

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './App.css';
-import SearchResults from '../SearchResults/SearchResults.js';
+import {SearchResults} from '../SearchResults/SearchResults.js';
 
 //COMPONENTS:
 /*
