866a343bb3381c02355571d924065474190e28da
DaVinci <ing_sergio.l@hotmail.com>
2022-12-06 03:23:58 -0600

    step 32

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,19 +25,20 @@ class App extends React.Component {
     };
   }
 
-
-  return (
+  render()
+  {return(
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
       <div className="App">
         {/* <SearchBar /> */}
         <div className="App-playlist">
-          {/* <SearchResults /> */}
+          <SearchResults searchResults={this.state.searchResults}/>
           {/* <Playlist /> */}
         </div>
       </div>
-    </div>
-  );
+    </div>);
+  }
+  
 
 }
 
